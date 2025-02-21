a8f797092dbe96d64945cca1110465e6ddc6ee7a
Muzuvukile Nqwiliso <muzuvukile.nqwiliso@sambeconsulting.com>
2025-02-21 09:28:15 +0200

    removed the SELECT command that I used here instead of INSERT

--- DatabaseAdministrator/SQLStatement/INSERT STATEMENT.sql
+++ DatabaseAdministrator/SQLStatement/INSERT STATEMENT.sql
@@ -1,3 +1,6 @@
-SELECT * FROM Employees
-WHERE Department = 'HR'
-AND Salary > 50000 AND Salary < 60000
+INSERT INTO Customers(FirstName,LastName,Email,City) 
+VALUES 
+('Thabo', 'Mbeki', 'thabo.mbeki@example.com', 'Johannesburg'),
+('Jan', 'Van der Merwe', 'jan.vdm@example.com', 'Pretoria'),
+('Nomvula', 'Mokonyane', 'nomvula.mokonyane@example.com', 'Durban'),
+('Lerato', 'Ndlovu', 'lerato.ndlovu@example.com', 'Cape Town');
