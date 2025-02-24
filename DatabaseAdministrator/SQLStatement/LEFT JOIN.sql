558abce6b84845e53c826312a6dfbf314b955615
Muzuvukile Nqwiliso <muzuvukile.nqwiliso@sambeconsulting.com>
2025-02-24 03:13:12 +0200

    added questions

--- DatabaseAdministrator/SQLStatement/LEFT JOIN.sql
+++ DatabaseAdministrator/SQLStatement/LEFT JOIN.sql
@@ -13,6 +13,17 @@ SELECT * FROM production.products
 SELECT * FROM production.categories
 SELECT * FROM production.stocks
 
+
+  -- ================================
+-- ?? left join practice questions
+-- ================================
+
+-- 6. get a list of all stores and their staff members, including stores that currently have no staff.
+-- 7. retrieve a list of all customers and their orders, including those who have never placed an order.
+-- 8. show a list of all staff members and their managers, including staff who don’t have a manager.
+-- 9. get a list of all products and their sales orders, including products that have never been ordered.
+-- 10. list all stores and the products they stock, including stores that do not have any products in stock.
+
 -- TASKS
 -- e.g 1: Tables - stores , staffs
 SELECT [str].*,stff.first_name as [staff name]
