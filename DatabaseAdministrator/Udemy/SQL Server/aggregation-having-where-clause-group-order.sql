67b08d6f04b504cc3f59232d39071bf19722892b
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-03-13 21:03:12 +0200

    modified the aggrigation... file by adding new scripts and comments

--- DatabaseAdministrator/Udemy/SQL Server/aggregation-having-where-clause-group-order.sql
+++ DatabaseAdministrator/Udemy/SQL Server/aggregation-having-where-clause-group-order.sql
@@ -1,21 +1,47 @@
+SELECT * FROM production.brands
+SELECT * FROM production.categories
+SELECT * FROM production.products
+SELECT * FROM production.stocks
 
-SELECT CustomerID, COUNT(OrderID) AS TotalOrders, SUM(TotalPrice) AS TotalSpent
-FROM store.Orders
-WHERE TotalPrice > 100
-GROUP BY CustomerID
-ORDER BY TotalSpent DESC;
+SELECT * FROM sales.customers
+SELECT * FROM sales.orders
+SELECT * FROM sales.order_items
+
+-- WHERE clause
+SELECT product_name,category_id,list_price 
+FROM production.products
+WHERE list_price >= 500
+
+-- ORDER BY
+SELECT * FROM production.products
+ORDER BY product_id DESC -- this desc means the tample data or items will be arranged in descending order 
+
+SELECT * FROM production.products
+ORDER BY product_id ASC -- this asc means the tample data or items will be arranged in ascending order 
+
+-- GROUP BY
+SELECT category_id AS each_category, 
+SUM(list_price) AS total_price_for_each_category
+FROM production.products
+GROUP BY category_id -- the group by must be before order by 
+ORDER BY category_id ASC 
+
+-- using group by with join
+SELECT c.first_name, 
+       c.email, 
+       items.product_id AS products,
+       SUM(items.quantity) AS total_items_ordered,
+       COUNT(items.quantity) AS number_items
+FROM sales.customers c
+INNER JOIN sales.orders ord ON c.customer_id = ord.customer_id
+INNER JOIN sales.order_items items ON ord.order_id = items.order_id
+GROUP BY c.first_name, c.email, items.product_id;
 
 
-SELECT ProductID, SUM(Stock) AS TotalStock
-FROM store.Products
-GROUP BY ProductID
-HAVING SUM(Stock) > 50
-ORDER BY TotalStock ASC;
-
-
-SELECT CustomerID, AVG(TotalPrice) AS AvgOrderPrice
-FROM store.Orders
-WHERE OrderDate >= '2025-01-01'
-GROUP BY CustomerID
-HAVING AVG(TotalPrice) > 500
-ORDER BY AvgOrderPrice DESC;
+-- HAVING 
+SELECT category_id AS each_category, 
+SUM(list_price) AS total_price_for_each_category
+FROM production.products
+GROUP BY category_id -- the group by must be before order by 
+HAVING SUM(list_price) = 98985.44 
+ORDER BY category_id ASC 
