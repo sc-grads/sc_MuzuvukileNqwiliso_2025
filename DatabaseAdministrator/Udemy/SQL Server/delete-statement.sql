e3dc02af6108442d676ac7fb3f621c87f54eb614
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-03-14 05:18:40 +0200

    modified delete file, by adding new script lines

--- DatabaseAdministrator/Udemy/SQL Server/delete-statement.sql
+++ DatabaseAdministrator/Udemy/SQL Server/delete-statement.sql
@@ -1,4 +1,9 @@
-DELETE FROM store.Orders
-WHERE TotalPrice < 50;
+DELETE FROM sales.orders
+WHERE sales.orders.order_date = CAST('2016-01-01');
 
-SELECT * FROM store.Orders
+SELECT * FROM sales.orders
+
+SELECT * FROM sales.orders
+
+DELETE FROM sales.customers
+WHERE sales.customers.customer_id = (SELECT * FROM sales.orders  WHERE sales.orders.customer_id = 2)
