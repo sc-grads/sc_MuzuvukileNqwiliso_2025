970d87199eaee689356b35a4d652d559f255aac3
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-05 21:12:15 +0200

    Refactor main.py to include additional print statements and variable definitions

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,8 @@
-print("Hello Bob")
+print("Hello Bob") # This is a test
+print("Hello Alice") # This is a test
+#Hello World
+
+name = "Alice"
+age = 30
+print(f"Hello {name}, you are {age} years old.") # This is a test
+print("Hello Bob") # This is a test
