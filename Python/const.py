83a529d494e2c5deba7a6e6ca49f8bd19a4417c9
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-05 21:16:52 +0200

    learning constants in python which are variabes that are // not meant to be changed. They are usually defined in all caps // with underscores separating words. Constants are used to // represent fixed values that should not be modified throughout

---
+++ Python/const.py
@@ -0,0 +1,11 @@
+PI = 3.14
+print("This is a constant file")
+
+print(4* PI)
+print(round(4 ** PI,2)) # This is a Exponentiation
+print(4 / PI) # This is a Division
+print(4 // PI) # This is a Integer Division
+print(4 % PI) # This is Modulus
+print(4 + PI) # This is Addition
+print(4 - PI) # This is Subtraction
+print(4 * PI) # This is Multiplication
