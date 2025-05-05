46fe3e9b46a37502a79f3de9e7b1540b7769f09b
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-05 21:21:46 +0200

    Add dataType.py to demonstrate various numeric, boolean, string, and collection types

---
+++ Python/dataType.py
@@ -0,0 +1,20 @@
+# Numeric types
+# int, float, complex
+number = 10
+print(type(number))  #  This is going to <class 'int'>
+float_number = 10.5
+print(type(float_number))  # This is going to <class 'float'>
+
+# Boolean type
+print(type(True))  # This is going to <class 'bool'>
+print(type(False))  # This is going to <class 'bool'>
+# String type
+print(type("Hello"))  # This is going to <class 'str'>
+print(type('Hello'))  # This is going to <class 'str'>
+# str
+# list, tuple, range
+numbers = [1, 2, 3, 4, 5]
+print(type(numbers))  # This is going to <class 'list'>
+numbers = (1, 2, 3, 4, 5)
+print(type(numbers))  # This is going to <class 'tuple'>
+numbers = range(1, 10)
