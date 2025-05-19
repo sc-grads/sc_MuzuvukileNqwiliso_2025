c2c3d5cd3cac9243afa78d519d55716c99782a11
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-19 09:26:30 +0200

    created a first file called first_file.sql

---
+++ DatabaseAdministrator/T-SQL/First_File.sql
@@ -0,0 +1,8 @@
+DECLARE @name VARCHAR(50)
+DECLARE @employer VARCHAR(50)
+
+SET @name = 'Mzu'
+SET @employer = 'SambeConsulting'
+
+PRINT 'My name is ' + @name + '. I''m working as a Data Analyst at ' + @employer
+GO
