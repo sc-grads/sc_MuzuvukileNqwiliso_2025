9f4539a8630bcaab056fa082a0eb0ebd91181da7
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-21 15:20:15 +0200

    Add SQL scripts for constraints, views, triggers, and transaction handling

---
+++ DatabaseAdministrator/T-SQL/Views.sql
@@ -0,0 +1,30 @@
+select 1
+go
+create view ViewByDepartment as 
+select D.Department, T.EmployeeNumber, T.DateOfTransaction, T.Amount as TotalAmount
+from tblDepartment as D
+left join tblEmployee as E
+on D.Department = E.Department
+left join tblTransaction as T
+on E.EmployeeNumber = T.EmployeeNumber
+where T.EmployeeNumber between 120 and 139
+--order by D.Department, T.EmployeeNumber
+go
+
+create view ViewSummary as 
+select D.Department, T.EmployeeNumber as EmpNum, sum(T.Amount) as TotalAmount
+from tblDepartment as D
+left join tblEmployee as E
+on D.Department = E.Department
+left join tblTransaction as T
+on E.EmployeeNumber = T.EmployeeNumber
+group by D.Department, T.EmployeeNumber
+--order by D.Department, T.EmployeeNumber
+go
+select * from ViewByDepartment where TotalAmount >1  order by  EmployeeNumber asc
+select Department, EmployeeNumber, TotalAmount
+from ViewByDepartment
+update ViewByDepartment set TotalAmount = 2.77 where EmployeeNumber = 132
+select * from ViewSummary
+
+delete from ViewByDepartment where EmployeeNumber = 123
