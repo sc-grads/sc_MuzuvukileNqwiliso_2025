c260fc2728fda88ece2bdc6c4a36cac2938d4586
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-05-26 05:53:16 +0200

    Add nonclustered indexes and unique constraint for tblDepartment

--- DatabaseAdministrator/T-SQL/INDEX_2.sql
+++ DatabaseAdministrator/T-SQL/INDEX_2.sql
@@ -26,3 +26,21 @@ SELECT
 FROM sys.indexes i
 INNER JOIN sys.dm_db_index_usage_stats s ON i.object_id = s.object_id AND i.index_id = s.index_id
 WHERE OBJECT_NAME(i.object_id) IN ('tblEmployee', 'tblEmployee2', 'myTable');
+
+create nonclustered index idx_tblEmployee_DateOfBirth on [dbo].[tblEmployee]([DateOfBirth])
+create nonclustered index idx_tblEmployee_DateOfBirth_Department on [dbo].[tblEmployee]([DateOfBirth],Department)
+
+drop index idx_tblEmployee on [dbo].[tblEmployee]
+
+select * from [dbo].[tblEmployee2] where [EmployeeNumber] = 127
+select * from [dbo].[tblEmployee2]
+
+select DateOfBirth, Department
+from [dbo].[tblEmployee]
+where DateOfBirth>='1992-01-01' and DateOfBirth<'1993-01-01'
+
+--seek = few number of rows based on the index
+--scan = going through the entire table
+
+alter table [dbo].[tblDepartment]
+add constraint unq_tblDepartment UNIQUE(Department)
