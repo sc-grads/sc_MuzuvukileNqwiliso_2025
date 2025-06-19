1ffc00f8703b21bc7f92f665b5477a796030f53d
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-06-19 11:13:59 +0200

    Fix SQL syntax for time interval in cleanup job for AuditLog and ErrorLog

--- HandsOnProject/Timesheet/SQL Scripts/CreateSqlAgentJob.sql
+++ HandsOnProject/Timesheet/SQL Scripts/CreateSqlAgentJob.sql
@@ -41,10 +41,10 @@ EXEC msdb.dbo.sp_add_jobstep
     @subsystem = 'TSQL',
     @command = N'
 DELETE FROM [TimesheetDB].[Timesheet].[AuditLog]
-WHERE [ProcessedDate] < DATEADD(MINUTES, -4, GETDATE());
+WHERE [ProcessedDate] < DATEADD(MINUTE, -4, GETDATE());
 
 DELETE FROM [TimesheetDB].[Timesheet].[ErrorLog]
-WHERE [ProcessedDate] < DATEADD(MINUTES, -4, GETDATE());
+WHERE [ProcessedDate] < DATEADD(MINUTE, -4, GETDATE());
 ',
     @database_name = 'TimesheetDB',
     @on_success_action = 1,
