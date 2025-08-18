e8fbdc94054b8a9427cd901a9205e9b5ee381de5
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-08-18 11:31:02 +0200

    feat: Add Product class and integrate product listing in Program.cs

--- Udemy/Collections/Program.cs
+++ Udemy/Collections/Program.cs
@@ -4,39 +4,45 @@
     {
         static void Main(string[] args)
         {
-            var colors = new List<string> { "red", "blue", "green", "yellow", "red", "purple", "orange", "red" };
 
 
-            foreach (var color in colors)
+
+            // Product list
+            var products = new List<Product>
             {
-                Console.WriteLine("Current Items:   "+  color);
-            }
+                new Product { Name = "Apple", Price = 1.2m },
+                new Product { Name = "Banana", Price = 0.8m },
+                new Product { Name = "Cherry", Price = 2.5m }
+            };
 
-            // Remove "red" safely using a for loop (backwards)
-            for (int i = colors.Count - 1; i >= 0; i--)
+            foreach (var product in products)
             {
-                if (colors[i] == "red")
-                {
-                    colors.RemoveAt(i);
-                }
-            }
-
-            string [] colors2 = colors.ToArray();
-            for (int i = 0; i < colors2.Length; i++)
-            {
-                Console.WriteLine("Array color : "+colors2[i]);
+                Console.WriteLine($"Product: {product.Name}, Price: {product.Price:C}");
             }
 
 
-            var myNumbers = new List<int> { 10,6,45,54,2,35,23,23,65};
+
+           
+
+            var myNumbers = new List<int> {1,2,3,4,5,6,7,8,15,25};
 
             Console.WriteLine("Predicates  numbers");
-            List<int> evenNumbers = myNumbers.FindAll(n => n % 2 == 0);
+            Predicate<int> isEven = n => n % 2 == 0;
+            List<int> evenNumbers = myNumbers.FindAll(isEven);
             List<int> oddNumbers = myNumbers.FindAll(n => n % 2 != 0);
             int firstEvenNumber = myNumbers.Find(n=> n % 2 == 0); // you must declare a varaiable as a type not as a List <int> because Find returns a single item compared to FindAll which returns a List of items
 
+            int isTwo = myNumbers.Find(n => n == 2);
+            
+            Console.WriteLine("Number 2 is found "+ isTwo);
+
             Console.WriteLine("Even numbers:");
             Console.Write("even numbers => ");
+
+            bool largeTtwenty = myNumbers.Any(n => n > 20);
+
+            
+
             foreach (var number in evenNumbers)
             {
                  Console.Write(number + " ");
@@ -47,18 +53,37 @@
             {
                 Console.Write(oddNumber + " ");
             }
+           
             Console.WriteLine();
-            Console.WriteLine("Before sorting:");
-            foreach (var number in myNumbers)
+            if (largeTtwenty)
             {
-                Console.WriteLine(number);
+                Console.WriteLine("There is a number larger than 20");
+            }
+            else
+            {
+                Console.WriteLine("There is no number larger than 20");
             }
 
-            myNumbers.Sort();
-            Console.WriteLine("After sorting:");
-            foreach (var number in myNumbers)
+            // using LINQ to filter and sort
+
+            var filteredNumbers = myNumbers
+                .Where(myNumbers => myNumbers > 10)
+                .Order()
+                .ToList();
+            // this returns a list of numbers larger than 20 and sorts them in ascending order
+
+            var numbersGreaterThanTen = myNumbers.Where(n => n > 10).OrderDescending().ToList();
+
+            Console.WriteLine("Numbers greater than 10 in ascending order:");
+
+            foreach (var number in numbersGreaterThanTen)
             {
-                Console.WriteLine(number);
+                Console.WriteLine($"Numbers greater than 10 : {number}");
+            }
+
+            foreach (var number in filteredNumbers)
+            {
+                Console.WriteLine($"Filtered number: {number}");
             }
 
         }
